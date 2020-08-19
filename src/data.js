c95797d96862b113b1936238b21bf2cd04bd8173
acer <rverdufiguera@gmail.com>
2020-08-19 11:17:10 -0400

    Conflicto de fusion resuelto nos quedamos con ambos cambios

--- src/data.js
+++ src/data.js
@@ -3,3 +3,27 @@
 export const example = () => 'example';
 
 export const anotherExample = () => 'OMG';
+
+
+function ordenarA_Z(ordenar) {
+    return ordenar.sort((a, b) => a.name.localeCompare(b.name));
+    // Llama a la función y le envía toda la data con un sort de a a-z
+}
+
+/* Filtro Casas Hogwarts*/
+
+export const housesHogwarts = data.filter(function(data) {
+    return data.house !== '';
+});
+
+export const gryffindor = data.filter(function(data) {
+    return data.house === "Gryffindor"
+});
+
+export const hufflepuf = data.filter(function(data) {
+    return data.house === "Hufflepuff"
+});
+
+export const ravenclaw = data.filter(function(data) {
+    return data.house === "Ravenclaw"
+});
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import data from './data/potter/potter.js';
 
 // Cabecera
 let cabeceraDinamica = document.getElementById("cabecera")
-cabeceraDinamica.innerHTML +=`
+cabeceraDinamica.innerHTML += `
 <div class="container"> 
 <header class="header2">
         <div>
@@ -19,17 +19,30 @@ cabeceraDinamica.innerHTML +=`
             <a target="_blank" href="">Videos</a>
             <a target="_blank" href="https://www.wizardingworld.com/">Conoce mas!</a>
             <a target="_blank" href="https://www.wizardingworld.com/about-the-fan-club">Club de Fans</a>
-          </nav>
+          
+        <select name="" id="seleccion">Ordenar
+      <option value="az">De la A a la Z</option>
+      </select>
+      <option value="az">De la A a la Z</option>
+      </select>
+            <select id="casas">
+                <option value="">Casas</option>
+                <option value="Gryffindor"> Gryffindor</option>
+                <option value="Slytherin"> Slytherin</option>
+                <option value="Ravenclaw"> Ravenclaw</option>
+                <option value="Hufflepuff"> Hufflepuff</option>
+              </select>
+            </nav>
         </div>
       </div>
       </header>
 </div>`
 
-// Segunda Pantalla mostrar personaje
 const mostrarPersonajes = () => {
+    // realizo el recorrido con un for de la nueva variable para verificar que se guardo bien y semuestra la data
     for (let i = 0; i <= data.length; i++) {
-            let namePersonaje = document.getElementById("name");
-                namePersonaje.innerHTML +=`
+        let namePersonaje = document.getElementById("name");
+        namePersonaje.innerHTML += `
             <div class="wrap">
               <ul>
                 <div class="tarjeta-wrap">              
@@ -47,12 +60,45 @@ const mostrarPersonajes = () => {
                   </div>
               </ul>
             </div>`
-          }
-        }
+    }
+}
 
+//  mostrarPersonajes();
 let boton = document.getElementById("btn-personajes")
 boton.addEventListener("click", mostrarPersonajes)
 
-document.getElementById("btn-personajes").onclick = function () {
-  documente.getElementById("primeraPantalla").style.display = "none"
+// Establecer variable de ID para utilizarlos en Addeventlistener
+
+const casas = document.getElementById('casas'); /*Variable que contiene a la ventana de personajes que mostrara data en HTML*/
+const ordenar = document.getElementById("root");
+
+document.getElementById("container").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    document.getElementById("casas").style.display = "none";
+    document.getElementById("root").style.display = "block";
+});
+
+/*Función para mostrar información relacionada al select de Casas*/
+
+const casasHarryPotter = (data) => {
+
+    for (let i = 0; i < data.length; i++) {
+        casas.innerHTML += `
+  <div class="card">
+  <div class= "cardFull">
+  <div class="front">
+  <div class ="imagenes"><img src=${data[i].image} ></div>
+  <h1>${data[i].name}</h1>
+  </div>
+  <div class="back">
+  <div class ="imagenlogo"><img src=${data[i].house}></div>
+  </div>
+  </div>
+  </div>
+  `
+    }
 }
+
+
+
+/*Función para mostrar información relacionada al select de Casas*/
