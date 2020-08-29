03412d2c3c4fca11c7f35194fc0186cb461f2bbb
acer <rverdufiguera@gmail.com>
2020-08-29 04:20:28 -0400

    Cambios agregados en el main.js y el nuevo html

--- data.js
+++
@@ -1,5 +0,0 @@
-// estas funciones son de ejemplo
-export const filterHouse = (house, data) => {
-    let typeFilterHouse = data.filter(element => element.house.includes(house));
-    return typeFilterHouse;
-}
--- main.js
+++ main.js
@@ -1,48 +1,12 @@
 //import { example } from 
 // './data.js';
 
-import { filterHouse } from './data.js';
-// aqui va todo lo del DOM para imprimir TODO  lo que queremos mostrar en el HTML
-// aqui recorro toda la data sin filtrar ni ordenar, solo mostrar informacion e imagen 
-// Importar las funciones que creamos en el data.js
-// './data.js';importar  datos  de  './data/potter/potter.js' ;
-// aqui recorro toda la data sin filtrar ni ordenar, solo mostrar informacion e imagen 
-// Importar las funciones que creamos en el data.js
-export const filterHouse = (house, data) => {
-        let typeFilterHouse = data.filter(element => element.house.includes(house));
-        return typeFilterHouse;
-    }
-    // './data.js';
-
+// './data.js';
 import data from './data/potter/potter.js';
-
-// Cabecera
-let cabeceraDinamica = document.getElementById("cabecera")
-cabeceraDinamica.innerHTML += `
-<div class="container"> 
-<header class="header2">
-        <div>
-          <div class="wrapper">
-          <input class="logo" type="image" src="./imagenes/Logo wizarding.png" id="RegresarPrimeraPantalla"/>
-          </div>
-        </div>
-        <div class="menu">
-          <nav>
-            <a href="#" id="btn-personajes">Personajes</a>
-            <a target="_blank" href="https://www.dondeir.com/cine-y-tv/donde-ver-todas-las-peliculas-de-harry-potter-en-linea/2020/06/">Descargables</a> <!--el atributo target sirve para abrir una pagina fuera de la pagina de nosotras --></li>
-            <a target="_blank" href="">Videos</a>
-            <a target="_blank" href="https://www.wizardingworld.com/">Conoce mas!</a>
-            <a target="_blank" href="https://www.wizardingworld.com/about-the-fan-club">Club de Fans</a>
-            </nav>
-        </div>
-      </div>
-      </header>
-</div>`
-
 const mostrarPersonajes = () => {
     // realizo el recorrido con un for de la nueva variable para verificar que se guardo bien y semuestra la data
     for (let i = 0; i <= data.length; i++) {
-        let namePersonaje = document.getElementById("name");
+        let namePersonaje = document.getElementById("container-personajes");
         namePersonaje.innerHTML += `
             <div class="wrap">
               <ul>
@@ -61,31 +25,128 @@ const mostrarPersonajes = () => {
                   </div>
               </ul>
             </div>`
-            // realizo el recorrido con un for de la nueva variable para verificar que se guardo bien y semuestra la data
-        for (let i = 0; i <= data.length; i++) {
 
-            let namePersonaje = document.getElementById("name");
+    }
+}
 
-            namePersonaje.innerHTML += `
-                <div class='main'>
-                <ul>
-                <div>
-                <br><br>
-                <img src =${data[i].image}></div>
-                <br><br>
-                <li><h2>Nombre en la saga:</h2></li>
-                <li><h2>${data[i].name}</h2></li>
-                <li><h3>Tipo de patronus: ${data[i].patronus}</h3></li>
-                <li><h3>Nombre del actor: ${data[i].actor}</h3></li>
-                <li><h3>Fecha de nacimiento: ${data[i].dateOfBirth}</h3></li>
-                </ul>
-                </div>`
-                // con el + se esta concatenando cada iteracion y muestre de uno en uno 
-                // en este caso se declara la funcion que esta guardada en el html
-                //})
+//  mostrarPersonajes();
+let boton = document.getElementById("btn-personajes")
+boton.addEventListener("click", mostrarPersonajes)
+
+// Funcion para mostrar personajes de Gryffindor
+
+const mostrarPersonajesGryffindor = () => {
+    for (let i = 0; i <= data.length; i++) {
+        if (data[i].house === "Gryffindor") {
+            let personajesGryffindor = document.getElementById("container-personajes");
+            personajesGryffindor.innerHTML += `
+  <div class="wrap">
+    <ul>
+      <div class="tarjeta-wrap">              
+        <div class="tarjeta">
+          <br>
+            <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
+            <div class="atras">
+              <br>
+              <p>Tipo de casa: ${data[i].house}</p>
+              <p>Nombre en la saga:</p>
+              <p>${data[i].name}</p>
+            </div>
+        </div>
+    </ul>
+  </div>`
+
         }
     }
-    //  mostrarPersonajes();
-    let boton = document.getElementById("btn-personajes")
-    boton.addEventListener("click", mostrarPersonajes)
 }
+
+const botonGryffindor = document.getElementById("Entra_gryffindor");
+botonGryffindor.addEventListener("click", mostrarPersonajesGryffindor);
+// Funcion para mostrar personajes de Slytherin
+const mostrarPersonajesSlytherin = () => {
+    for (let i = 0; i <= data.length; i++) {
+        if (data[i].house === "Slytherin") {
+            let personajesSlytherin = document.getElementById("container-personajes");
+            personajesSlytherin.innerHTML += `
+<div class="wrap">
+  <ul>
+    <div class="tarjeta-wrap">              
+      <div class="tarjeta">
+        <br>
+          <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
+          <div class="atras">
+            <br>
+            <p>Tipo de casa: ${data[i].house}</p>
+            <p>Nombre en la saga:</p>
+            <p>${data[i].name}</p>
+          </div>
+      </div>
+  </ul>
+</div>`
+
+        }
+    }
+}
+
+const botonSlytherin = document.getElementById("Entra_slytherin");
+botonSlytherin.addEventListener("click", mostrarPersonajesSlytherin);
+
+// Funcion para mostrar personajes de hufflepuff
+
+const mostrarPersonajesHufflepuff = () => {
+    for (let i = 0; i <= data.length; i++) {
+        if (data[i].house === "Hufflepuff") {
+            let personajesHufflepuff = document.getElementById("container-personajes");
+            personajesHufflepuff.innerHTML += `
+<div class="wrap">
+  <ul>
+    <div class="tarjeta-wrap">              
+      <div class="tarjeta">
+        <br>
+          <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
+          <div class="atras">
+            <br>
+            <p>Tipo de casa: ${data[i].house}</p>
+            <p>Nombre en la saga:</p>
+            <p>${data[i].name}</p>
+          </div>
+      </div>
+  </ul>
+</div>`
+
+        }
+    }
+}
+
+const botonHufflepuff = document.getElementById("Entra_hufflepuff");
+botonHufflepuff.addEventListener("click", mostrarPersonajesHufflepuff);
+
+// Funcion para mostrar personajes de Revenclaw
+
+const mostrarPersonajesRavenclaw = () => {
+    for (let i = 0; i <= data.length; i++) {
+        if (data[i].house === "Ravenclaw") {
+            let personajesRavenclaw = document.getElementById("container-personajes");
+            personajesRavenclaw.innerHTML += `
+<div class="wrap">
+<ul>
+  <div class="tarjeta-wrap">              
+    <div class="tarjeta">
+      <br>
+        <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
+        <div class="atras">
+          <br>
+          <p>Tipo de casa: ${data[i].house}</p>
+          <p>Nombre en la saga:</p>
+          <p>${data[i].name}</p>
+        </div>
+    </div>
+</ul>
+</div>`
+
+        }
+    }
+}
+
+const botonRavenclaw = document.getElementById("Entra_ravenclaw");
+botonRavenclaw.addEventListener("click", mostrarPersonajesRavenclaw);
