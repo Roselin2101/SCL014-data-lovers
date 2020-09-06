9f9f37aa9bf49bfcb851e5aabf323f453b17ea1a
acer <rverdufiguera@gmail.com>
2020-09-05 23:21:13 -0400

    OK

--- main.js
+++ main.js
@@ -1,80 +1,43 @@
-//import { example } from 
-// './data.js';
-
-// './data.js';
 import data from './data/potter/potter.js';
+
 const mostrarPersonajes = () => {
-    // realizo el recorrido con un for de la nueva variable para verificar que se guardo bien y semuestra la data
-    for (let i = 0; i <= data.length; i++) {
-        let namePersonaje = document.getElementById("container-personajes");
-        namePersonaje.innerHTML += `
-            <div class="wrap">
-              <ul>
-                <div class="tarjeta-wrap">              
-                  <div class="tarjeta">
-                    <br>
-                      <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
-                      <div class="atras">
-                        <br>
-                        <p>Nombre en la saga:</p>
-                        <p>${data[i].name}</p>
-                        <p>Tipo de patronus: ${data[i].patronus}</p>
-                        <p>Nombre del actor: ${data[i].actor}</p>
-                        <p>Fecha de nacimiento: ${data[i].dateOfBirth}</p>
-                      </div>
-                  </div>
-              </ul>
-            </div>`
+  for (let i = 0; i <= data.length; i += 1) {
+    const namePersonaje = document.getElementById('container-personajes');
+    namePersonaje.innerHTML += `<div class='wrap'>
+   <ul>
+   <div class='tarjeta-wrap'>
 
-    }
-}
-
-//  mostrarPersonajes();
-let boton = document.getElementById("btn-personajes")
-boton.addEventListener("click", mostrarPersonajes)
+   <div class='tarjeta'>
+   <br>
+   <div class='adelante'><img src =${data[i].image} width="130" height="130"></</div>
+   <div class="atras">
+   <br>
+   <p>Tipo de patronus: ${data[i].patronus}</p>
+   <p>Nombre del actor: ${data[i].actor}</p>
+   <p>Fecha de nacimiento: ${data[i].dateOfBirth}</p>
+   </div>
+   </div>
+   </ul>
+   </div>`;
+  }
+};
+const boton = document.getElementById('btn-personajes');
+boton.addEventListener('click', mostrarPersonajes);
 
 // Funcion para mostrar personajes de Gryffindor
 
 const mostrarPersonajesGryffindor = () => {
-    for (let i = 0; i <= data.length; i++) {
-        if (data[i].house === "Gryffindor") {
-            let personajesGryffindor = document.getElementById("container-personajes");
-            personajesGryffindor.innerHTML += `
-  <div class="wrap">
-    <ul>
-      <div class="tarjeta-wrap">              
-        <div class="tarjeta">
-          <br>
-            <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
-            <div class="atras">
-              <br>
-              <p>Tipo de casa: ${data[i].house}</p>
-              <p>Nombre en la saga:</p>
-              <p>${data[i].name}</p>
-            </div>
-        </div>
-    </ul>
-  </div>`
-
-        }
-    }
-}
-
-const botonGryffindor = document.getElementById("Entra_gryffindor");
-botonGryffindor.addEventListener("click", mostrarPersonajesGryffindor);
-// Funcion para mostrar personajes de Slytherin
-const mostrarPersonajesSlytherin = () => {
-    for (let i = 0; i <= data.length; i++) {
-        if (data[i].house === "Slytherin") {
-            let personajesSlytherin = document.getElementById("container-personajes");
-            personajesSlytherin.innerHTML += `
-<div class="wrap">
-  <ul>
-    <div class="tarjeta-wrap">              
-      <div class="tarjeta">
-        <br>
-          <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
-          <div class="atras">
+  for (let i = 0; i <= data.length; i += 1) {
+    if (data[i].house === 'Gryffindor') {
+      const personajesGryffindor = document.getElementById('container-personajes');
+      personajesGryffindor.innerHTML += `
+<div class='wrap'>
+<ul>
+ <div class='tarjeta-wrap'>
+ <div class='tarjeta'>
+ <br>
+ <div class='adelante'><img src =${data[i].image} width="100" height="100"></</div>
+          <div class='atras'>
             <br>
             <p>Tipo de casa: ${data[i].house}</p>
             <p>Nombre en la saga:</p>
@@ -82,55 +45,51 @@ const mostrarPersonajesSlytherin = () => {
           </div>
       </div>
   </ul>
-</div>`
-
-        }
+</div>`;
     }
-}
+  }
+};
+const botonGryffindor = document.getElementById('Entra_gryffindor');
+botonGryffindor.addEventListener('click', mostrarPersonajesGryffindor);
 
-const botonSlytherin = document.getElementById("Entra_slytherin");
-botonSlytherin.addEventListener("click", mostrarPersonajesSlytherin);
+// Funcion para mostrar personajes de Slytherin
+
+const mostrarPersonajesSlytherin = () => {
+  for (let i = 0; i <= data.length; i += 1) {
+    if (data[i].house === 'Slytherin') {
+      const personajesSlytherin = document.getElementById('container-personajes');
+      personajesSlytherin.innerHTML += `
+<div class='wrap'>
+<ul>
+<div class='tarjeta-wrap'>
+<div class='tarjeta'>
+<br>
+<div class='adelante'><img src =${data[i].image} width="130" height="130"></</div>
+<div class='atras'>
+  <br>
+  <p>Tipo de casa: ${data[i].house}</p>
+  <p>Nombre en la saga:</p>
+  <p>${data[i].name}</p>
+    </div>
+    </div>
+</ul>
+</div>`;
+    }
+  }
+};
+const botonSlytherin = document.getElementById('Entra_slytherin');
+botonSlytherin.addEventListener('click', mostrarPersonajesSlytherin);
 
 // Funcion para mostrar personajes de hufflepuff
 
 const mostrarPersonajesHufflepuff = () => {
-    for (let i = 0; i <= data.length; i++) {
-        if (data[i].house === "Hufflepuff") {
-            let personajesHufflepuff = document.getElementById("container-personajes");
-            personajesHufflepuff.innerHTML += `
-<div class="wrap">
-  <ul>
-    <div class="tarjeta-wrap">              
-      <div class="tarjeta">
-        <br>
-          <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
-          <div class="atras">
-            <br>
-            <p>Tipo de casa: ${data[i].house}</p>
-            <p>Nombre en la saga:</p>
-            <p>${data[i].name}</p>
-          </div>
-      </div>
-  </ul>
-</div>`
-
-        }
-    }
-}
-
-const botonHufflepuff = document.getElementById("Entra_hufflepuff");
-botonHufflepuff.addEventListener("click", mostrarPersonajesHufflepuff);
-
-// Funcion para mostrar personajes de Revenclaw
-
-const mostrarPersonajesRavenclaw = () => {
-    for (let i = 0; i <= data.length; i++) {
-        if (data[i].house === "Ravenclaw") {
-            let personajesRavenclaw = document.getElementById("container-personajes");
-            personajesRavenclaw.innerHTML += `
+  for (let i = 0; i <= data.length; i += 1) {
+    if (data[i].house === 'Hufflepuff') {
+      const personajesHufflepuff = document.getElementById('container-personajes');
+      personajesHufflepuff.innerHTML += `
 <div class="wrap">
 <ul>
-  <div class="tarjeta-wrap">              
+  <div class="tarjeta-wrap">
     <div class="tarjeta">
       <br>
         <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
@@ -142,27 +101,37 @@ const mostrarPersonajesRavenclaw = () => {
         </div>
     </div>
 </ul>
-</div>`
-
-        }
+</div>`;
     }
-}
-
-const botonRavenclaw = document.getElementById("Entra_ravenclaw");
-botonRavenclaw.addEventListener("click", mostrarPersonajesRavenclaw);
-
-//// ORDENAR ALFABETICAMENTE  DE A a Z (array.sort(function(a, b){return b-a}); )
-const ordenarAz = (data) => {
-    const nuevoArraydata = [];
-    for (let i = 0; i < data.length; i += 1) {
-        nuevoArraydata.push(data[i]);
-        nuevoArraydata.sort((a, b) => {
-            // comparamos  2 elementos  nombre si a es mayor a b retorna -1 y se coloca antes de b
-            if (a.name < b.name) { return -1; }
-            if (a.name > b.name) { return 1; }
-            return 0;
-
-        });
-    }
-    return nuevoArraydata;
+  }
 };
+const botonHufflepuff = document.getElementById('Entra_hufflepuff');
+botonHufflepuff.addEventListener('click', mostrarPersonajesHufflepuff);
+
+// Funcion para mostrar personajes de Revenclaw
+
+const mostrarPersonajesRavenclaw = () => {
+  for (let i = 0; i <= data.length; i += 1) {
+    if (data[i].house === 'Ravenclaw') {
+      const personajesRavenclaw = document.getElementById('container-personajes');
+      personajesRavenclaw.innerHTML += `
+<div class="wrap">
+<ul>
+<div class="tarjeta-wrap">
+  <div class="tarjeta">
+    <br>
+      <div class="adelante"><img src =${data[i].image} width="130" height="130"></</div>
+      <div class="atras">
+        <br>
+        <p>Tipo de casa: ${data[i].house}</p>
+        <p>Nombre en la saga:</p>
+        <p>${data[i].name}</p>
+      </div>
+  </div>
+</ul>
+</div>`;
+    }
+  }
+};
+const botonRavenclaw = document.getElementById('Entra_ravenclaw');
+botonRavenclaw.addEventListener('click', mostrarPersonajesRavenclaw);
